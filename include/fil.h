30d460aa26b0893f5658d5902bd2ee76133e17be
WuX <wux@thenest.dev>
2026-04-19 07:31:01 +0300

    docs: document the public header

--- include/fil.h
+++ include/fil.h
@@ -13,6 +13,17 @@ typedef enum {
     FIL_RESULT_NOMEM,
 } FilResult;
 
+/*
+ * Function pointer to a function that expands the buffer.
+ *
+ * Takes a pointer to the current data allocation and
+ * a size for the new allocation. The pointer may be a
+ * nullptr.
+ *
+ * Returns a pointer to the existing or a new allocation
+ * with the new size. The new allocation must contain the
+ * data from the original allocation.
+ */
 typedef void *(*FilBufferExpandFn)(void *cur, size_t size);
 
 typedef struct {
@@ -37,8 +48,33 @@ typedef struct {
 FilStream fil_stream_new(const void *data, size_t data_size);
 FilBitStream fil_bitstream_new(FilStream *stream);
 
+/*
+ * Create a buffer of static size.
+ *
+ * Note that if the decompressed data does not
+ * fit within the buffer an error will be returned.
+ */
 FilBuffer fil_buffer_new_static(void *buffer, size_t buffer_size);
+
+/**
+ * Create a buffer of dynamic size.
+ *
+ * This buffer will expand as needed.
+ *
+ * Note that at the end of decompression the `length` field of
+ * the buffer indicates actual size of decompressed data.
+ *
+ * Also, remember to free the data buffer :)
+ */
 FilBuffer fil_buffer_new_dynamic(FilBufferExpandFn expand);
 
+/**
+ * Decompress `input_stream` to `output_buffer` using
+ * the deflate algorithm.
+ */
 FilResult fil_deflate(FilBitStream *input_stream, FilBuffer *output_buffer);
+
+/**
+ * Decompress a GZIP in `input_stream` to `output_buffer`.
+ */
 FilResult fil_gzip_decompress(FilStream *input_stream, FilBuffer *output_buffer);
